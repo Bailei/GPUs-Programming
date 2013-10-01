6f283a425c25c266ea403beb112185092d475b57
Bailei <taofeng311@gmail.com>
2013-10-01 05:25:39 -0400

    optimizing

--- vecAdd-cuda.cu
+++ vecAdd-cuda.cu
@@ -2,8 +2,6 @@
 #include<stdlib.h>
 #include<cuda_runtime.h>
 
-#define W 5000000
-
 bool InitCUDA(){
 	int count;
 	cudaDeviceProp prop;
@@ -49,16 +47,17 @@ int main(){
 		return 0;
 	}
 	printf("CUDA initialized.\n");
-
-	clock_t start = clock();
-	float* A, *B, *C;
-	A = (float*) malloc(sizeof(int) * W);
-	B = (float*) malloc(sizeof(int) * W);
-	C = (float*) malloc(sizeof(int) * W);
-	vector_gen(A, W);
-	vector_gen(B, W);
-	
-	int size = W * sizeof(float);
+    
+    int i;
+    for(i = 100; i < 200000000; i *= 2){
+    clock_t start = clock();
+    float* A, *B, *C;
+	A = (float*) malloc(sizeof(float) * i);
+	B = (float*) malloc(sizeof(float) * i);
+	C = (float*) malloc(sizeof(float) * i);
+	vector_gen(A, i);
+	vector_gen(B, i);
+    int size = i * sizeof(float);
 	float* A_d, *B_d, *C_d;
 
 	cudaMalloc((void**) &A_d, size);
@@ -66,26 +65,17 @@ int main(){
 	cudaMalloc((void**) &B_d, size);
 	cudaMemcpy(B_d, B, size, cudaMemcpyHostToDevice);
 	cudaMalloc((void**) &C_d, size);
-
+    
 	dim3 dimBlock(512, 1);
-	dim3 dimGrid(ceil(float(W)/512), 1);
-
-	vecAddkernel<<<dimGrid, dimBlock>>>(A_d, B_d, C_d, W);
-	clock_t end = clock() - start;
+	dim3 dimGrid(ceil(float(i)/512), 1);
+	vecAddkernel<<<dimGrid, dimBlock>>>(A_d, B_d, C_d, i);
 
 	cudaMemcpy(C, C_d, size, cudaMemcpyDeviceToHost);
 	cudaFree(A_d);
 	cudaFree(B_d);
 	cudaFree(C_d);
-
-	for(int i = 0; i < W ; i++){
-        if(i % 400  == 0){
-            //printf("\n");
-            //printf("\n");
-        }
-        //printf("%f ", C[i]);
-	}
-	printf("\n");
-	printf("time: %ldms\n", end);
+	clock_t end = (clock() - start) / 1000;
+	printf("Length %d vector addion use time: %ldms\n", i, end);
+    }
 	return 0;
 }
--- vecAdd-seq.c
+++ vecAdd-seq.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define W 51200
+float* A, *B, *C;
 
 void vector_gen(float* a, int size){
 	int i;
@@ -11,35 +11,26 @@ void vector_gen(float* a, int size){
 	}
 }
 
-void vecAdd(float* A, float* B, float* C){
+void vecAdd(float* A, float* B, float* C, int x){
 	int i;
-	for(i = 0; i < W; i++){
+	for(i = 0; i < x; i++){
 		C[i] = A[i] + B[i];
 	}
 }
 
 int main(){
-	clock_t start, time_used;
-	start = clock();
-	float* A, *B, *C;
-	A = (float*) malloc(sizeof(int) * W);
-	B = (float*) malloc(sizeof(int) * W);
-	C = (float*) malloc(sizeof(int) * W);
-	vector_gen(A, W);
-	vector_gen(B, W);
-
-	vecAdd(A, B, C);
-	time_used = clock() - start;
-
 	int i;
-	for(i = 0; i < W ; i++){
-        if(i % 10  == 0){
-            printf("\n");
-        }
-        printf("%f ", C[i]);
-	}
-	printf("\n");
-	printf("time: %ldms\n", time_used);
+    for(i = 100; i < 200000000; i *= 2){
+    clock_t start = clock();
+    A = (float*) malloc(sizeof(float) * i);
+	B = (float*) malloc(sizeof(float) * i);
+	C = (float*) malloc(sizeof(float) * i);
+    vector_gen(A, i);
+	vector_gen(B, i);
+	vecAdd(A, B, C, i);
+	clock_t end = (clock() - start) / 1000;
+	printf("Length %d vector addion use time: %ldms\n", i, end);
+    }
 	return 0;
 
 }
